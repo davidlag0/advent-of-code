33b189a229e5e438bf3a25d55e67c630fc907d69
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2021-05-20 09:06:10 -0400

    Refactor to reduce number or mypy and pylint errors

--- 2020/day_3/day3.py
+++ 2020/day_3/day3.py
@@ -11,19 +11,19 @@ def load_grid_information(input_file):
         'column_length': None,
     }
     row_number = 0
-    
-    with open(input_file, 'r') as input:
-        for line in input:
+
+    with open(input_file, 'r') as file:
+        for line in file:
             char_pos = 0
-            
+
             for char in line.strip():
                 if char == '#':
                     grid_info['coordinates'].add((char_pos, row_number))
-                
+
                 char_pos += 1
-            
+
             row_number += 1
-            
+
     grid_info['row_length'] = char_pos
     grid_info['column_length'] = row_number
 
@@ -31,18 +31,18 @@ def load_grid_information(input_file):
 
 def get_number_of_trees(slope):
     number_of_trees = 0
-    
-    for y in range(1, grid['column_length']):
-        x = slope * y
-        
-        if x >= grid['row_length']:
-            x_tile = x % grid['row_length']
+
+    for y_coord in range(1, grid['column_length']):
+        x_coord = slope * y_coord
+
+        if x_coord >= grid['row_length']:
+            x_tile = x_coord % grid['row_length']
         else:
-            x_tile = x
-        
-        if (x_tile, y) in grid['coordinates']:
+            x_tile = x_coord
+
+        if (x_tile, y_coord) in grid['coordinates']:
             number_of_trees += 1
-            
+
     return number_of_trees
 
 if __name__ == '__main__':
@@ -53,4 +53,3 @@ if __name__ == '__main__':
 
     # Part 2
     print(f'Part 2: {functools.reduce(mul, [get_number_of_trees(slope) for slope in [1, 3, 5, 7, 1/2]], 1)}')
-    
