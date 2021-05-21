af19fb41dbd3aa53cbfd9973f0312f7c79ab925f
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2021-05-20 23:06:01 -0400

    Add solution for part 2

--- 2020/day_14/day14.py
+++ 2020/day_14/day14.py
@@ -63,16 +63,82 @@ zero - 101 (at address 7) and 64 (at address 8) - producing a sum of 165.
 
 Execute the initialization program. What is the sum of all values left in memory after it
 completes? (Do not truncate the sum to 36 bits.)
+
+--- Part Two ---
+
+For some reason, the sea port's computer system still can't communicate with your ferry's
+docking program. It must be using version 2 of the decoder chip!
+
+A version 2 decoder chip doesn't modify the values being written at all. Instead, it acts
+as a memory address decoder. Immediately before a value is written to memory, each bit in
+the bitmask modifies the corresponding bit of the destination memory address in the following
+way:
+
+    If the bitmask bit is 0, the corresponding memory address bit is unchanged.
+    If the bitmask bit is 1, the corresponding memory address bit is overwritten with 1.
+    If the bitmask bit is X, the corresponding memory address bit is floating.
+
+A floating bit is not connected to anything and instead fluctuates unpredictably. In practice,
+this means the floating bits will take on all possible values, potentially causing many memory
+addresses to be written all at once!
+
+For example, consider the following program:
+
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+
+When this program goes to write to memory address 42, it first applies the bitmask:
+
+address: 000000000000000000000000000000101010  (decimal 42)
+mask:    000000000000000000000000000000X1001X
+result:  000000000000000000000000000000X1101X
+
+After applying the mask, four bits are overwritten, three of which are different, and two of
+which are floating. Floating bits take on every possible combination of values; with two
+floating bits, four actual memory addresses are written:
+
+000000000000000000000000000000011010  (decimal 26)
+000000000000000000000000000000011011  (decimal 27)
+000000000000000000000000000000111010  (decimal 58)
+000000000000000000000000000000111011  (decimal 59)
+
+Next, the program is about to write to memory address 26 with a different bitmask:
+
+address: 000000000000000000000000000000011010  (decimal 26)
+mask:    00000000000000000000000000000000X0XX
+result:  00000000000000000000000000000001X0XX
+
+This results in an address with three floating bits, causing writes to eight memory addresses:
+
+000000000000000000000000000000010000  (decimal 16)
+000000000000000000000000000000010001  (decimal 17)
+000000000000000000000000000000010010  (decimal 18)
+000000000000000000000000000000010011  (decimal 19)
+000000000000000000000000000000011000  (decimal 24)
+000000000000000000000000000000011001  (decimal 25)
+000000000000000000000000000000011010  (decimal 26)
+000000000000000000000000000000011011  (decimal 27)
+
+The entire 36-bit address space still begins initialized to the value 0 at every address, and
+you still need the sum of all values left in memory at the end of the program. In this example,
+the sum is 208.
+
+Execute the initialization program using an emulator for a version 2 decoder chip. What is the
+sum of all values left in memory after it completes?
 '''
 
 from __future__ import annotations
 import os
-from typing import Iterable, Dict
+from typing import Iterable, Dict, List, Iterator, Match
 import unittest
 import re
+import math
 
 
 TEST_INPUT_FILENAME = 'day_14_test_input.txt'
+TEST_INPUT_FILENAME2 = 'day_14_test_input2.txt'
 INPUT_FILENAME = 'day_14_input.txt'
 
 
@@ -87,18 +153,54 @@ class Program:
     mask_pattern = re.compile(r'mask\s=\s([X01]{36})')
     memwrite_pattern = re.compile(r'mem\[(\d+)\]\s=\s(\d+)')
 
-    def __init__(self) -> None:
+    def __init__(self, version_one: bool = True) -> None:
         self.__mask = 'X' * 36
         self.__memory: Dict[int, int] = dict()
+        self.__version_one = version_one
+
+    def apply_v2_mask(self, memory_slot: int) -> str:
+        '''Return a masked address'''
+        result_address: str = ''
+
+        for (address_char, mask_char) in zip('{:36b}'.format(memory_slot), self.mask):
+            if address_char == '1' and mask_char == '0':
+                result_address += '1'
+            else:
+                result_address += mask_char
+
+        return result_address
+
+    def resolve_address(self, masked_address: str) -> List[int]:
+        '''Given a masked address, return memory addresses ready for writing'''
+        addresses: List[int] = []
+        number_of_x: int = masked_address.count('X')
+        number_of_addresses = int(math.pow(2, number_of_x))
+
+        for specific_address in range(number_of_addresses):
+            binary_address: str = '{0:b}'.format(specific_address).zfill(number_of_x)
+            address_iterator: Iterator[str] = iter(binary_address)
+
+            # pylint: disable=unused-argument
+            def replace(match: Match[str]) -> str:
+                # pylint: disable=cell-var-from-loop
+                return next(address_iterator)
+
+            addresses.append(int(re.sub('X', replace, masked_address), 2))
+
+        return addresses
 
     def read_program(self, instruction: str) -> None:
         '''Read a program line and call the proper action to be executed'''
         if match := re.match(self.mask_pattern, instruction):
             self.__mask = match.group(1)
         if match := re.match(self.memwrite_pattern, instruction):
-            value = int(match.group(2)) | int(self.mask.replace('X', '0'), 2)
-            value = value & int(self.mask.replace('X', '1'), 2)
-            self.set_memory(int(match.group(1)), value)
+            if self.version_one:
+                value = int(match.group(2)) | int(self.mask.replace('X', '0'), 2)
+                value = value & int(self.mask.replace('X', '1'), 2)
+                self.set_memory(int(match.group(1)), value)
+            else:
+                for mem_address in self.resolve_address(self.apply_v2_mask(int(match.group(1)))):
+                    self.set_memory(mem_address, int(match.group(2)))
 
     @property
     def mask(self) -> str:
@@ -108,6 +210,15 @@ class Program:
     @mask.setter
     def mask(self, value: str) -> None:
         self.__mask = value
+
+    @property
+    def version_one(self) -> bool:
+        '''Getter for version_one class member'''
+        return self.__version_one
+
+#    @version_one.setter
+#    def version_one(self, value: bool) -> None:
+#        self.__version_one = value
 
     def get_memory(self, memslot: int) -> int:
         '''Getter for memory class member'''
@@ -122,6 +233,13 @@ class Program:
         return self.__memory
 
 
+def solve(filename: str, version_one: bool = True) -> int:
+    '''Solve part 1 of the puzzle'''
+    program = Program(version_one=version_one)
+    for instruction in load_input_file(filename):
+        program.read_program(instruction)
+    return sum(program.dump_memory().values())
+
 class Tests(unittest.TestCase):
     '''Tests'''
 
@@ -132,6 +250,24 @@ class Tests(unittest.TestCase):
         ('mem[8] = 11', 8, 73),
         ('mem[7] = 101', 7, 101),
         ('mem[8] = 0', 8, 64)
+    )
+    part2_tests = (
+        (
+            'mask = 000000000000000000000000000000X1001X',
+            42,
+            '000000000000000000000000000000X1101X',
+            [26, 27, 58, 59],
+            'mem[42] = 100',
+            400
+        ),
+        (
+            'mask = 00000000000000000000000000000000X0XX',
+            26,
+            '00000000000000000000000000000001X0XX',
+            [16, 17, 18, 19, 24, 25, 26, 27],
+            'mem[26] = 1',
+            8
+        )
     )
 
     def test_set_mask(self) -> None:
@@ -168,20 +304,46 @@ class Tests(unittest.TestCase):
 
     def test_solve_part1_with_test_data(self) -> None:
         '''Test the solve_part1 function with test data'''
-        self.assertEqual(solve_part1(TEST_INPUT_FILENAME), 165)
+        self.assertEqual(solve(TEST_INPUT_FILENAME, version_one=True), 165)
 
+    def test_apply_v2_mask(self) -> None:
+        '''Test the apply_v2_mask function'''
 
-def solve_part1(filename: str) -> int:
-    '''Solve part 1 of the puzzle'''
-    program = Program()
-    for instruction in load_input_file(filename):
-        program.read_program(instruction)
-    return sum(program.dump_memory().values())
+        program = Program()
+
+        for case in self.part2_tests:
+            with self.subTest(case):
+                program.read_program(case[0])
+                self.assertEqual(program.apply_v2_mask(case[1]), case[2])
+
+    def test_resolve_address(self) -> None:
+        '''Test the resolve_address function'''
+
+        program = Program()
+
+        for case in self.part2_tests:
+            with self.subTest(case):
+                self.assertListEqual(program.resolve_address(case[2]), case[3])
+
+    def test_v2_read_program(self) -> None:
+        '''Test the read_program function for version_one = False'''
+
+        for case in self.part2_tests:
+            program = Program(version_one=False)
+
+            with self.subTest(case):
+                program.read_program(case[0])
+                program.read_program(case[4])
+                self.assertEqual(sum(program.dump_memory().values()), case[5])
+
+    def test_solve_part2_with_test_data(self) -> None:
+        '''Test the solve_part1 function with test data'''
+        self.assertEqual(solve(TEST_INPUT_FILENAME2, version_one=False), 208)
 
 
 if __name__ == '__main__': # pragma: no cover
     print('Running unit tests...')
     unittest.main(verbosity=2, exit=False)
     print('Puzzle Answers:')
-    print(f'Part 1: {solve_part1(INPUT_FILENAME)}')
-    print(f'Part 2: {0}')
+    print(f'Part 1: {solve(INPUT_FILENAME, version_one=True)}')
+    print(f'Part 2: {solve(INPUT_FILENAME, version_one=False)}')
